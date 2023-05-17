309af14299c317301b3a838e09a75941f8aa42e0
Huradil <nuradiltolokov@gmail.com>
2023-05-17 20:11:34 +0200

    admin hw

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,79 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tweet, Reaction, ReactionType, TweetImages,Reply,ReplyReaction
+
+
+@admin.display(description='Short Text')
+def get_short_text(obj):
+    return f'{obj.text[:20]}...'
+
+
+class TweetImagesInline(admin.TabularInline):
+    model = TweetImages
+    extra = 1
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    inlines = [
+        TweetImagesInline
+    ]
+    date_hierarchy = 'created_add'
+    actions_on_bottom = True
+    actions_on_top = False
+    empty_value_display = "--empty--"
+    # exclude = ['profile', 'image']
+    # fields = ['text', ]
+    fields = (('text','profile'),'image')
+    list_display = ['id','get_profile_fullname','get_reactions_str',get_short_text,'image','created_add']
+    list_display_links = [get_short_text, 'id']
+    list_editable = ['image', ]
+    list_filter = ['created_add','profile']
+    list_per_page = 2
+    save_as = True
+    save_on_top = True
+    search_fields = ['text','profile__user__username__exact']
+    sortable_by = ['created_add','id']
+
+    @admin.display(description='Fullname')
+    def get_profile_fullname(self,obj):
+        return obj.profile.user.get_full_name()
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ReactionType)
+class ReactionTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reply)
+class ReplayAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_add'
+    actions_on_bottom = True
+    actions_on_top = False
+    empty_value_display = "-"
+    fields = (('text','profile'),'tweet')
+    list_display = ['id','get_full_name',get_short_text,'created_add','get_reactions_str','profile']
+    list_display_links = [get_short_text,]
+    list_editable = ['profile']
+    search_fields = ['text','profile__user__username']
+    list_filter = ['created_add','id']
+    sortable_by = ['id','created_add']
+
+    @admin.display(description='fullname')
+    def get_full_name(self,obj):
+        return obj.profile.user.get_full_name()
+
+
+@admin.register(ReplyReaction)
+class ReplyReactionsAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,11 @@
+from django.contrib import admin
 from django.db import models
 
 from accounts.models import Profile
 
 
 def tweet_image_store(instance, filename):
-    return f'profile/{instance.profile.user.username}/{instance.created_add}'
+    return f'profile/{instance.profile.user.username}/{instance.created_add}/{filename}'
 
 
 class Tweet(models.Model):
@@ -15,6 +16,10 @@ class Tweet(models.Model):
     created_add = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Твит"
+        verbose_name_plural = "Твиты"
 
     def all_reactions(self):
         result={}
@@ -33,6 +38,11 @@ class Tweet(models.Model):
             else:
                 result[reaction.reaction.name]=1
         return result
+
+    @admin.display(description='reactions')
+    def get_reactions_str(self):
+        reactions = self.get_reactions()
+        return str(reactions)
 
     def __str__(self):
         return self.text
@@ -54,6 +64,11 @@ class Reply(models.Model):
             else:
                 result[reaction.reaction.name] = 1
         return result
+
+    @admin.display(description='reactions')
+    def get_reactions_str(self):
+        reactions=self.get_reactions()
+        return str(reactions)
 
     def __str__(self):
         return self.text
@@ -89,3 +104,12 @@ class ReplyReaction(models.Model):
 
     class Meta:
         unique_together=['reply','profile']
+
+
+def tweet_multiple_images_store(instance, filename):
+    return f'profile/{instance.tweet.profile.user.username}/{instance.tweet.id}/{filename}'
+
+
+class TweetImages(models.Model):
+    tweet = models.ForeignKey(Tweet,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=tweet_multiple_images_store)
